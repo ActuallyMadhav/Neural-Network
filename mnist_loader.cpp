178ddca828aa5d6a583b92507ed5bdc585460bfc
ActuallyMadhav <madhavra.2004@gmail.com>
2026-03-28 17:57:41 +0800

    changed project structure

--- mnist_loader.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include <vector>
-#include <fstream>
-#include <Eigen/Dense>
-#include "mnist_loader.h"
-
--- mnist_loader.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <Eigen/Dense>
-
-Eigen::MatrixXf load_images(const std::string &filepath);
-Eigen::MatrixXf load_labels(const std::string &filepath);
